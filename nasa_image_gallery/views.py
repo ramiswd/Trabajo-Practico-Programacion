8bfb1b6409e9bd0a04b2366fcafad1f7f00fda05
rami <ramisalv777@gmail.com>
2024-06-17 17:36:54 -0300

    inicio de sesion (terminado)

--- nasa_image_gallery/views.py
+++ nasa_image_gallery/views.py
@@ -1,10 +1,11 @@
 # capa de vista/presentación
 # si se necesita algún dato (lista, valor, etc), esta capa SIEMPRE se comunica con services_nasa_image_gallery.py
 
-from django.shortcuts import redirect, render # type: ignore
+from django.shortcuts import redirect, render
 from .layers.services import services_nasa_image_gallery
-from django.contrib.auth.decorators import login_required # type: ignore
-from django.contrib.auth import logout # type: ignore
+from django.contrib.auth.decorators import login_required 
+from django.contrib.auth import logout 
+
 
 # función que invoca al template del índice de la aplicación.
 def index_page(request):
@@ -19,6 +20,7 @@ def getAllImagesAndFavouriteList(request):
     return images, favourite_list
 
 # función principal de la galería.
+@login_required
 def home(request):
     # llama a la función auxiliar getAllImagesAndFavouriteList() y obtiene 2 listados: uno de las imágenes de la API y otro de favoritos por usuario*.
     # (*) este último, solo si se desarrolló el opcional de favoritos; caso contrario, será un listado vacío [].
@@ -41,7 +43,6 @@ def search(request):
     
     
     return render(request, 'home.html', {'images': images, 'favourite_list': favourite_list} )
-
 
 
 @login_required
